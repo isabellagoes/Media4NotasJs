fbd0e71cfdfd0e1b18a1e7cf010a7086eee942a5
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-22 19:28:49 -0300

    padronização de if else

--- script.js
+++ script.js
@@ -69,15 +69,24 @@ if (nota1 < 0 || nota1 > 10 ||
     nota3 < 0 || nota3 > 10 ||
     nota4 < 0 || nota4 > 10) {
     alert("Digite somente notas entre 0 e 10.");
-
-} else {
+    
+    }
+    else
+    {
     let media = ((nota1 + nota2 + nota3 + nota4) / 4).toFixed(2);
 
-    if (media >= 6 && media <= 10) {
+    if (media >= 6 && media <= 10)
+    {
         alert("Você ficou com média: " + media + ". Resultado: Aprovado(a)");
-    } else if (media >= 5 && media < 6) {
+    }
+
+    else if (media >= 5 && media < 6)
+    {
         alert("Você ficou com média: " + media + ". Resultado: Em recuperação");
-    } else if (media >= 0 && media < 5) {
+    }
+
+    else if (media >= 0 && media < 5)
+    {
         alert("Você ficou com média: " + media + ". Resultado: Reprovado(a)");
     }
 }
